62a7e7cba2308c958942d16ea6f7b8eff8e2e546
ma721kup <kupferschmidmathias@gmail.com>
2018-10-08 16:18:14 +0200

    added broker main class

--- aqua/blatt1/broker/Broker.java
+++ aqua/blatt1/broker/Broker.java
@@ -1,15 +1,20 @@
 package aqua.blatt1.broker;
 
+import aqua.blatt1.common.Direction;
+import aqua.blatt1.common.FishModel;
 import aqua.blatt1.common.msgtypes.DeregisterRequest;
+import aqua.blatt1.common.msgtypes.HandoffRequest;
+import aqua.blatt1.common.msgtypes.RegisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterResponse;
 import messaging.Endpoint;
 import messaging.Message;
 
 import java.io.Serializable;
+import java.net.InetSocketAddress;
 
 public class Broker {
 
-    private ClientCollection clientList  = new ClientCollection<>();
+    private ClientCollection<InetSocketAddress> clientList  = new ClientCollection<>();
 
     private Endpoint endpoint = new Endpoint(4711);
 
@@ -17,13 +22,27 @@ public class Broker {
    {
        while(true)
        {
+           System.out.println("anfrage");
            Message message =  endpoint.blockingReceive();
+
+            if(message.getPayload() instanceof RegisterRequest)
+            {
+                this.register(message);
+            }
+            else if ( message.getPayload() instanceof DeregisterRequest)
+            {
+                this.deregister(message);
+            }
+            else if( message.getPayload() instanceof HandoffRequest)
+            {
+                this.handoffFish(message);
+            }
        }
    }
 
    private void register(Message message)
    {
-       String id = "client" + clientList.size()+ 1;
+       String id = "client" + clientList.size();
        clientList.add(id,message.getSender());
        endpoint.send(message.getSender(),new RegisterResponse(id));
    }
@@ -34,4 +53,34 @@ public class Broker {
        DeregisterRequest request = (DeregisterRequest)payload;
        clientList.remove(clientList.indexOf(request.getId()));
    }
+
+    private void handoffFish (Message message)
+    {
+        int sourceindex = clientList.indexOf(message.getSender());
+        Serializable payload = message.getPayload();
+        HandoffRequest request = (HandoffRequest)payload;
+        FishModel fish = request.getFish();
+        if(fish.getDirection()== Direction.RIGHT)
+        {
+            if (sourceindex == clientList.size()- 1)
+            {
+                endpoint.send(clientList.getClient(0), message.getPayload());
+            }
+            else
+            {
+                endpoint.send(clientList.getClient(sourceindex + 1), message.getPayload());
+            }
+        }
+        else if (fish.getDirection() == Direction.LEFT)
+        {
+            if(sourceindex == 0)
+            {
+                endpoint.send(clientList.getClient(clientList.size() - 1),message.getPayload());
+            }
+            else
+            {
+                endpoint.send(clientList.getClient(sourceindex - 1), message.getPayload());
+            }
+        }
+    }
 }
---
+++ aqua/blatt1/broker/main.java
@@ -0,0 +1,8 @@
+package aqua.blatt1.broker;
+
+public class main {
+    public static void main(String[] args) {
+        Broker broker = new Broker();
+        broker.broker();
+    }
+}
